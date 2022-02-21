d53b589d746124459be0652b4652a12323e84d12
Sean Bailey <sean@actualsize.com>
2022-02-20 21:05:00 -0500

    completed Set One challenge three

--- src/lib.rs
+++ src/lib.rs
@@ -27,4 +27,45 @@ pub mod set_one {
 
         return hex::encode(&xor_buff).to_string();
     }
+
+    // const array of letter frequency in English text
+    // source: https://crypto.stackexchange.com/a/40930
+    // similar table also available on https://enwp.org/Letter_frequency
+    pub const LETTER_FREQUENCIES: [f32; 27] = [
+        0.0651738, 0.0124248, 0.0217339, 0.0349835, 0.1041442, 0.0197881, 0.0158610, 0.0492888,
+        0.0558094, 0.0009033, 0.0050529, 0.0331490, 0.0202124, 0.0564513, 0.0596302, 0.0137645,
+        0.0008606, 0.0497563, 0.0515760, 0.0729357, 0.0225134, 0.0082903, 0.0171272, 0.0013692,
+        0.0145984, 0.0007836, 0.1918182, // Y, Z, space
+    ];
+
+    // determines the likelihood of a string to be English or not
+    // implements the Bhattacharyya coefficient using letters and space
+    pub fn englishness(sample: &str) -> f32 {
+        let arg = sample.to_ascii_uppercase();
+        // short-circuit if string is empty
+        if arg.len() == 0 {
+            return 0.0;
+        };
+        // get frequency table of sample string
+        let mut count: [i32; 27] = [0; 27];
+        // first, count characters
+        for c in arg.chars() {
+            // TODO: refactor to use .fold and a hashmap
+            // see https://stackoverflow.com/q/60660277
+            match c {
+                'A'..='Z' => count[c as usize - 65] += 1,
+                ' ' => count[26] += 1,
+                _ => (),
+            }
+        }
+        // then, convert from letter counts to frequencies
+        let freq: [f32; 27] = count.map(|x| x as f32 / arg.len() as f32);
+
+        // now we calculate the sum of squares
+        let coefficient = freq
+            .iter()
+            .enumerate()
+            .fold(0.0, |acc, (i, x)| acc + (x * LETTER_FREQUENCIES[i]).sqrt());
+        return coefficient;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -17,4 +17,52 @@ fn main() {
         ),
         "746865206b696420646f6e277420706c6179"
     );
+
+    // CHALLENGE THREE
+    set_one_challenge_three();
+}
+
+// input has been XOR'd with one character (assumption: ASCII character)
+// figure out which one it is, and crack the plaintext
+fn set_one_challenge_three() {
+    const INPUT: &str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    let input_len: usize = hex::decode(INPUT).unwrap().len();
+
+    #[derive(Debug)]
+    struct Candidate {
+        plaintext: String,
+        key: char,
+        score: f32,
+    }
+    let mut output = Vec::new();
+
+    // step one: XOR input with every ASCII character
+    // and calculate its "englishness" score
+    for c in 0..=127 as u8 {
+        // repeat test key character `input_len` times
+        let test_key = &std::iter::repeat(c as char)
+            .take(input_len)
+            .collect::<String>();
+
+        // get options
+        let val = hex::decode(matasano::set_one::fixed_xor(INPUT, &hex::encode(test_key))).unwrap();
+        let decoded = String::from_utf8(val).unwrap();
+        let englishness = matasano::set_one::englishness(&decoded);
+
+        let c = Candidate {
+            plaintext: decoded,
+            key: c as char,
+            score: englishness,
+        };
+        output.push(c);
+    }
+
+    // sort solutions by englishness
+    output.sort_by(|a, b| b.score.partial_cmp(&a.score).unwrap());
+
+    // print best solution
+    println!(
+        "Your best solution is: '{}', with key '{}' and Englishness score of {}.",
+        output[0].plaintext, output[0].key, output[0].score
+    );
 }
